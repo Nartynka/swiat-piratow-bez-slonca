22929446784b82ccf567f3a9e51daf8dc4d10a47
Martyna Plutowska <57597187+Nartynka@users.noreply.github.com>
2022-04-24 15:29:33 +0200

    Fix the bug where a quest is automatically completed before starting if player has meet requirements for completing the quest

--- misc/Quest.gd
+++ misc/Quest.gd
@@ -1,12 +1,10 @@
 extends Node
 
 """
-Instance this as a child of any Npc.tscn node and it turns into a 
-quest giver.
+Instance this as a child of any Npc.tscn node and it turns into a quest giver.
 
 The character will also check if the player has a certain amount of a given item and if true
 Remove said amount from the players inventory and give some amount of another item as a reward
-You can also ask for '5 demon horns' or something to turn it into a kill quest.
 """
 
 export(String) var quest_name = "Test Quest"
@@ -21,23 +19,32 @@ export(String) var pending_dialog = "Test Quest Pending"
 export(String) var delivered_dialog = "Test Quest Complete"
 
 var is_in_dialog = false
-var quest_status
+var quest_status = Quest.STATUS.NONEXISTENT
+
+func start_quest():
+	var is_quest_new = Quest.accept_quest(quest_name)
+	if is_quest_new:
+		start_dialog(initial_dialog)
+	else:
+		process()
+	
 func process():
 	quest_status = Quest.get_status(quest_name)
 	match quest_status:
 		Quest.STATUS.NONEXISTENT:
-			Quest.accept_quest(quest_name)
-			start_dialog(initial_dialog)
+			start_quest()
 		Quest.STATUS.STARTED:
-			print(Quest.get_status(quest_name))
 			if Inventory.get_item(required_item) >= required_amount:
-				Inventory.remove_item(required_item, required_amount)
 				Quest.change_status(quest_name, Quest.STATUS.COMPLETE)
-				Inventory.add_item(reward_item, reward_amount)
-				get_parent().quest_list.pop_front()
-				start_dialog(delivered_dialog)
+				process()
 			else:
 				start_dialog(pending_dialog)
+		Quest.STATUS.COMPLETE:
+			Inventory.remove_item(required_item, required_amount)
+			Inventory.add_item(reward_item, reward_amount)
+			get_parent().quest_list.pop_front()
+			start_dialog(delivered_dialog)
+			#print(quest_name+" completed")
 		_:
 			return
 
