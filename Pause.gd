c3300c65823f92cbfc6f3f11d8009b9857f1fdbb
acualesa0 <99009386+acualesa0@users.noreply.github.com>
2022-05-25 22:14:33 +0200

    Last commit

---
+++ Pause.gd
@@ -0,0 +1,21 @@
+extends Control
+
+var is_paused = false setget set_is_paused
+
+func _unhandled_input(event):
+	if event.is_action_pressed("pause"):
+		self.is_paused = !is_paused
+
+func set_is_paused(value):
+	is_paused = value
+	get_tree().paused = is_paused
+	visible = is_paused
+
+
+func _on_resumegamebtn_pressed():
+	self.is_paused = false
+
+func _on_quitbsn_pressed():
+	get_tree().quit()
+
+
